5c166ec8ea6f4a1e28ad3e56a25e27da29bfb599
Gabriel Costa de Souza <86692993+GbrlSouza@users.noreply.github.com>
2024-07-16 15:21:23 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <meta http-equiv="refresh" content="5;/pages/home/index.php">
+        <meta http-equiv="refresh" content="5;./pages/home/page.php">
         <title>Service Port</title>
     </head>
 </html>
